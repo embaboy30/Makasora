fe73b5fe24c02fa9e01e87d0572baceac4bdcdfc
mari <mari@quantumcrowd.io>
2020-08-02 20:15:45 +0800

    feature: Continuation on crash course

---
+++ src/app/http.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  myMethod() {
+    return console.log("Hey, what's up!");
+  }
+
+  getBeer() {
+    return this.http.get('http://api.openbrewerydb.org/breweries');
+  }
+}
