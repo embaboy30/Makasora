1bc2c3e84c92dd66aa6f5bc363b117697136ea2e
mari <mari@quantumcrowd.io>
2020-08-02 21:47:16 +0800

    feature: added additional tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { LearnComponent } from './learn/learn.component';
+import { PopularComponent } from './popular/popular.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'list', component: ListComponent },
+  {path: 'reviews', component: ReviewsComponent },
+  {path: 'learn', component: LearnComponent },
+  {path: 'popular', component: PopularComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { LearnComponent } from './learn/learn.component';
+import { PopularComponent } from './popular/popular.component';
 import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
+    ReviewsComponent,
+    LearnComponent,
+    PopularComponent
   ],
   imports: [
     BrowserModule,
